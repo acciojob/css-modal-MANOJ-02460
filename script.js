51f137c4d8ac049e7c1221ceed61679ca4657adc
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-09-07 18:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 //your JS code here. If required.
 
 let openModalBtn = document.getElementById("openModal");
-let divModal = document.querySelector("modal");
-let closeBtn = document.
+let divModal = document.querySelector(".modal");
+let closeBtn = document.querySelector(".close-modal");
+
+openModalBtn.addEventListener("click",function(){
+	divModal.style.display = "block"
+})
+
+closeBtn.addEventListener("click",function(){
+	divModal.style.display = "none"
+})
+
+divModal.addEventListener("click",function(){
+	divModal.style.display = "none"
+})
+
+
